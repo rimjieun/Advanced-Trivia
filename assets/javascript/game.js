404387bccfc802f03ab35db937e74269893c8a89
Jieun Rim <rim.jieun@gmail.com>
2017-03-04 08:27:45 -0500

    ready for submission

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,35 +5,94 @@ $(document).ready(function() {
 	var unanswered = 0;
 	var timer;
 	var count;
-	var isCorrect; // boolean
+	var isCorrect;
+	var questAnsTemp = [];
 
 	var questionAnswers = [
-		{question: "Q1", choices: ["A", "B", "bunny", "D"], answer: "bunny"},
-		{question: "Q2", choices: ["A", "B", "cat", "D"], answer: "cat"},
-		{question: "Q3", choices: ["A", "B", "hello", "D"], answer: "hello"},
-		{question: "Q4", choices: ["A", "B", "C", "D"], answer: "C"},
-		{question: "Q5", choices: ["A", "B", "C", "D"], answer: "C"},
-		{question: "Q6", choices: ["A", "B", "C", "D"], answer: "C"}
+		{
+			question: "Who is goddess of vengeance?",
+			choices: ["NEMESIS", "ANTHEIA", "CETO", "HARMONIA"],
+			answer: "NEMESIS",
+			image: "assets/images/nemesis.jpg"
+		},
+		{
+			question: "Who is goddess of rainbows?",
+			choices: ["MANIA", "METIS", "IRIS", "HERA"],
+			answer: "IRIS",
+			image: "assets/images/iris.gif"
+		},
+		{
+			question: "Who is god of the woods?",
+			choices: ["PAN", "HEBE", "MOMUS", "KRATOS"],
+			answer: "PAN",
+			image: "assets/images/pan.gif"
+		},
+		{
+			question: "Who is goddess of nature?",
+			choices: ["NYX", "RHEA", "ELECTRA", "PLUTUS"],
+			answer: "RHEA",
+			image: "assets/images/rhea.png"
+		},
+		{
+			question: "Who is god of death?",
+			choices: ["MAIA", "HADES", "NEREUS", "THANATOS"],
+			answer: "THANATOS",
+			image: "assets/images/thanatos.jpg"
+		},
+		{
+			question: "Who is goddess of victory?",
+			choices: ["HESTIA", "DORIS", "ERIS", "NIKE"],
+			answer: "NIKE",
+			image: "assets/images/nike.jpg"
+		},
+		{
+			question: "Who is goddess of Earth?",
+			choices: ["EROS", "GAIA", "OCEANUS", "PALLAS"],
+			answer: "GAIA",
+			image: "assets/images/gaia.jpg"
+		},
+		{
+			question: "Who is god of darkness?",
+			choices: ["URANUS", "TRITON", "EREBUS", "ZEUS"],
+			answer: "EREBUS",
+			image: "assets/images/erebus.jpg"
+		},
+		{
+			question: "Who is god of time?",
+			choices: ["POSEIDON", "CHRONOS", "HERACLES", "KRONOS"],
+			answer: "CHRONOS",
+			image: "assets/images/chronos.jpg"
+		},
+		{
+			question: "Who is god of war?",
+			choices: ["ARES", "BIA", "EUTERPE", "DEMETER"],
+			answer: "ARES",
+			image: "assets/images/ares.jpg"
+		}
 	];
 
 
 	var message = {
-		correct: function() {
-			$("#question-section").append("Correct!");
-			$("#question-section").append("<div><img src='assets/images/cat.jpg'></div>");
+		correct: function(current) {
+			$("#question-section").append("<div><img src='" + current.image + "'></div>");
+			$("#question-section").append("<div class='message'><p>Correct</p></div>");
+			
 		},
-		incorrect: function(answer) {
-			$("#question-section").append("Incorrect");
-			$("#question-section").append("<div>The correct answer was: " + answer + "</div>");
-			$("#question-section").append("<div><img src='assets/images/cat.jpg'></div>");
+		incorrect: function(current) {
+			$("#question-section").append("<div><img src='" + current.image + "'></div>");
+			$("#question-section").append("<div class='message'><p>Incorrect</p><div>");
+			$(".message").append("<p>Answer: " + current.answer + "</p>");
+			
 		},
-		outOfTime: function(answer) {
-			$("#question-section").append("Out of time!");
-			$("#question-section").append("<div>The correct answer was: " + answer + "</div>");
-			$("#question-section").append("<div><img src='assets/images/cat.jpg'></div>");
+		outOfTime: function(current) {
+			$("#question-section").append("<div><img src='" + current.image + "'></div>");
+			$("#question-section").append("<div class='message'><p>Out of time</p></div>");
+			$(".message").append("<p>Answer: " + current.answer + "</p>");
+			
 		}
 	}
 
+	// Event handlers-------------------------------------------------------------------------------
 
 	$("#start-button").on("click", function() {
 		loadQuestion();
@@ -48,93 +107,84 @@ $(document).ready(function() {
 	function loadQuestion() {
 		var current = questionAnswers[0];
 		timer = setInterval(countDown, 1000);
-		count = 5;
-		$("#game-section").html("<div id='timer'>" + count + "</div>");
+		count = 30;
+		$("#game-section").html("<div id='timer'><p>Time remaining: " + count + "</p></div>");
 		$("#game-section").append("<div id='question-section'></div>");
-		$("#question-section").append("<div id='question'>" + current.question + "</div>");
-		$("#question").append("<ul id='choices'></ul>");
+		$("#question-section").append("<p id='question'>" + current.question + "</p></div>");
+		$("#question-section").append("<ul id='choices'></ul>");
 		for (i = 0; i < current.choices.length; i++) {
 			$("#choices").append("<li value=" + current.choices[i] + ">" + current.choices[i] + "</li>");
 		}
 
 		$("li").on("click", function() {
 			clearInterval(timer);
-			checkAnswer(this, current.answer);
-			loadAnswer(current.answer);
+			checkAnswer(this, current);
+			loadAnswer(current);
 			loadNext();
 		});
 
 		function countDown() {
 			count--;
-			$("#timer").html(count);
+			$("#timer").html("<p>Time remaining: " + count + "</p>");
 			if (count === 0) {
 				isCorrect = false;
 				clearInterval(timer);
-				loadAnswer(current.answer);
+				loadAnswer(current);
 				loadNext();
 			}
 		}
 	}
 
-	function checkAnswer(choice, answer) {
-		if ($(choice).attr("value") === answer) {
-				isCorrect = true;
+	function checkAnswer(choice, current) {
+		if ($(choice).attr("value") === current.answer) {
+			isCorrect = true;
 		}
 		else {
-				isCorrect = false;
+			isCorrect = false;
 		}
 	}
 
-	function loadAnswer(answer) {
+	function loadAnswer(current) {
 		$("#question-section").empty();
 		if (isCorrect === true) {
 			correct++;
-			message.correct();
+			message.correct(current);
 		}
 		if ((count === 0) && (isCorrect === false)) {
 			unanswered++;
-			message.outOfTime(answer);
+			message.outOfTime(current);
 		}
 		if ((count > 0) && (isCorrect === false)) {
 			incorrect++;
-			message.incorrect(answer);
+			message.incorrect(current);
 		}
 	}
 
 	function loadNext() {
-		questionAnswers.shift();
+		questAnsTemp.push(questionAnswers.shift());
 		if (questionAnswers.length === 0) {
-			setTimeout(loadResults, 3000);
+			setTimeout(loadResults, 1000 * 4);
 		}
 		else {
-			setTimeout(loadQuestion, 1000);
+			setTimeout(loadQuestion, 1000 * 4);
 		}
 	}
 
 	function loadResults() {
 		$("#question-section").empty();
-		$("#question-section").append("<div id='result-msg'>All done! Here's how you did: </div>");
-		$("#result-msg").append("<ul id='results'></ul>");
-		$("#results").append("<li>Correct: " + correct + "</li>");
-		$("#results").append("<li>Incorrect: " + incorrect + "</li>");
-		$("#results").append("<li>Unanswered: " + unanswered + "</li>");
+		$("#question-section").append("<p id='results' class='result-msg'>All done! Here's how you did: </p>");
+		$("#results").append("<p class='result-msg'>Correct: " + correct + "</p>");
+		$("#results").append("<p class='result-msg'>Incorrect: " + incorrect + "</p>");
+		$("#results").append("<p class='result-msg'>Unanswered: " + unanswered + "</p>");
 		$("#game-section").append("<button id='start-over' type='button'>Start Over</button>");
 	}
 
 	function startOver() {
-
 		correct = 0;
 		incorrect = 0;
 		unanswered = 0;
-
-		questionAnswers = [
-			{question: "Q1", choices: ["A", "B", "bunny", "D"], answer: "bunny"},
-			{question: "Q2", choices: ["A", "B", "cat", "D"], answer: "cat"},
-			{question: "Q3", choices: ["A", "B", "hello", "D"], answer: "hello"},
-			{question: "Q4", choices: ["A", "B", "C", "D"], answer: "C"},
-			{question: "Q5", choices: ["A", "B", "C", "D"], answer: "C"},
-			{question: "Q6", choices: ["A", "B", "C", "D"], answer: "C"}
-		];
+		questionAnswers = questAnsTemp;
+		questAnsTemp = [];
 
 		loadQuestion();
 	}
